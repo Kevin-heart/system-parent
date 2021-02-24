1542636e585323736b85d10a3717550657b515cd
AllenHeart <2667275115@qq.com>
2021-02-24 21:26:35 +0800

    first commit

---
+++ system-admin/src/main/java/cn/shxnzw/Application.java
@@ -0,0 +1,24 @@
+package cn.shxnzw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项 目 名 称：cn.shxnzw
+ * 类 名 称：system-parent
+ * 创 建 人：huanghao
+ * 创建时间：2021/2/24 1:31 下午
+ * 类 描 述： TODO
+ * @version: V1.8
+ */
+
+/**
+ * 项目的启动入口
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
