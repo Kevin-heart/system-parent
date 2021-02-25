6dd0097bc918f8680526bfd1aa3df6560181df12
AllenHeart <2667275115@qq.com>
2021-02-25 09:54:05 +0800

    系统提供

---
+++ system-admin/src/main/java/cn/shxnzw/AppRun.java
@@ -0,0 +1,30 @@
+package cn.shxnzw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项 目 名 称：cn.shxnzw
+ * 类 名 称：system-parent
+ * 创 建 人：huanghao
+ * 创建时间：2021/2/25 9:45 上午
+ * 类 描 述： TODO
+ *
+ * @version: V1.8
+ */
+@SpringBootApplication
+public class AppRun {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
